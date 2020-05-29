080c56f7e635e7cf0517aea89fb58da7d80b6bbc
SolowinFXVI <julien.n.jacquet@protonmail.com>
2020-05-29 15:42:01 +0200

    Added Q4, cleaned up code

--- src/main.c
+++ src/main.c
@@ -9,12 +9,14 @@ int DEBUG_2 = 0; //advanced debug
 int DEBUG_FREES = 0;
 int DEBUG_READ = 0;
 
-double SEUIL = 0.000001;
+double SEUIL = 0.000001; //seuil pour la convergence
 static int nodeCount = 0;
 static int totalNodes = 0;
-static int shown = 0;
+static int shown = 0; //pour affichage du chargement du fichier
 
-
+/*
+* Affiche les éléments de la liste chainée d'une matrice
+*/
 void print_list(node_t * head){
     node_t * current = head;
     while (current != NULL) {
@@ -55,6 +57,12 @@ void print_list(node_t * head){
 	//~ }
 //}
 
+/*
+* Insert une valeur en deuxième position, juste après la node qui forme le tableau,
+* sauf si il n'y a pas de node dans le tableau, 
+* dans ce cas c'est inséré directement dans le tableau.
+* Affiche le chargement du fichier.
+*/
 void insert_head(node_t P[], double val, int column, int row){
     if(P[column].next == NULL){
         if(P[column].row == -1){ //vide
@@ -88,51 +96,58 @@ void insert_head(node_t P[], double val, int column, int row){
         newNode->val = val;
         newNode->next = P[column].next;
         P[column].next = newNode;
-    }    
+    }
+
+    //Affichage chargement du fichier
     nodeCount++;
+    {
         if(((nodeCount * 100)/totalNodes == 10) && (shown==0)){
-        printf("Loading 10%% \n");
-        shown++;
+            printf("Loading 10%% \n");
+            shown++;
+        }
+        else if(((nodeCount * 100)/totalNodes == 20) && (shown==1)){
+            printf("Loading 20%% \n");
+            shown++;
+        }
+        else if(((nodeCount * 100)/totalNodes == 30) && (shown==2)){
+            printf("Loading 30%% \n");
+            shown++;
+        }
+        else if(((nodeCount * 100)/totalNodes == 40) && (shown==3)){
+            printf("Loading 40%% \n");
+            shown++;
+        }
+        else if(((nodeCount * 100)/totalNodes == 50) && (shown==4)){
+            printf("Loading 50%% \n");
+            shown++;
+        }
+        else if(((nodeCount * 100)/totalNodes == 60) && (shown==5)){
+            printf("Loading 60%% \n");
+            shown++;
+        }
+        else if(((nodeCount * 100)/totalNodes == 70) && (shown==6)){
+            printf("Loading 70%% \n");
+            shown++;
+        }
+        else if(((nodeCount * 100)/totalNodes == 80) && (shown==7)){
+            printf("Loading 80%% \n");
+            shown++;
+        }
+        else if(((nodeCount * 100)/totalNodes == 90) && (shown==8)){
+            printf("Loading 90%% \n");
+            shown++;
+        }
+        else if(((nodeCount * 100)/totalNodes == 99) && (shown==9)){
+            printf("Loading 100%% \n");
+            shown++;
+        }
+        if(DEBUG_2)printf("filled existing node : %d \n", nodeCount);
     }
-    else if(((nodeCount * 100)/totalNodes == 20) && (shown==1)){
-        printf("Loading 20%% \n");
-        shown++;
-    }
-    else if(((nodeCount * 100)/totalNodes == 30) && (shown==2)){
-        printf("Loading 30%% \n");
-        shown++;
-    }
-    else if(((nodeCount * 100)/totalNodes == 40) && (shown==3)){
-        printf("Loading 40%% \n");
-        shown++;
-    }
-    else if(((nodeCount * 100)/totalNodes == 50) && (shown==4)){
-        printf("Loading 50%% \n");
-        shown++;
-    }
-    else if(((nodeCount * 100)/totalNodes == 60) && (shown==5)){
-        printf("Loading 60%% \n");
-        shown++;
-    }
-    else if(((nodeCount * 100)/totalNodes == 70) && (shown==6)){
-        printf("Loading 70%% \n");
-        shown++;
-    }
-    else if(((nodeCount * 100)/totalNodes == 80) && (shown==7)){
-        printf("Loading 80%% \n");
-        shown++;
-    }
-    else if(((nodeCount * 100)/totalNodes == 90) && (shown==8)){
-        printf("Loading 90%% \n");
-        shown++;
-    }
-    else if(((nodeCount * 100)/totalNodes == 99) && (shown==9)){
-        printf("Loading 100%% \n");
-        shown++;
-    }
-    if(DEBUG_2)printf("filled existing node : %d \n", nodeCount);
 }
 
+/*
+ * Récupère la taille de la matrice principale depuis le fichier. 
+*/
 int get_matrix_size(char * path){
     int matrix_size = -1;
     int dummy = -1;
@@ -147,6 +162,9 @@ int get_matrix_size(char * path){
     return matrix_size;
 }
 
+/*
+* Récupère le nombre total de nodes depuis le fichier.
+*/
 int get_totalNodes(char * path){
     int dummy = -1;
     int nodes = -1;
@@ -161,6 +179,9 @@ int get_totalNodes(char * path){
     return nodes;
 }
 
+/*
+* Initialise la matrice principale en lisant depuis le fichier.
+*/
 void init_matrix(int matrix_size,node_t P[],char * path){
     FILE *file = fopen(path,"r");
     if(file==NULL){
@@ -195,6 +216,9 @@ void init_matrix(int matrix_size,node_t P[],char * path){
     fclose(file);
 }
 
+/*
+* Affiche la matrice principale, l'affichage montre les colonnes en abscisses.
+*/
 void print_matrix(int matrix_x_size, int y,node_t P[]){
     printf("\n############## MATRIX ###############\n");
     for (int x = 0; x < matrix_x_size; x++)
@@ -205,6 +229,9 @@ void print_matrix(int matrix_x_size, int y,node_t P[]){
     printf("############## EOF MATRIX ###############\n");
 }
 
+/*
+* Réstitue la mémoire allouée pour les nodes de la matrice.
+*/
 void free_matrix(node_t P[], int matrix_size) {
     
     for (int i = matrix_size-1; i >= 0; i--) {
@@ -230,6 +257,9 @@ void free_matrix(node_t P[], int matrix_size) {
     }
 }
 
+/*
+* Met les valeurs du tableau de la matrice à 0.
+*/
 void zero_matrix(node_t P[], int matrix_size){
     for (int i = 0; i < matrix_size; i++)
     {
@@ -240,10 +270,17 @@ void zero_matrix(node_t P[], int matrix_size){
     }
 }
 
+/*
+* Allocation de la matrice.
+*/
 node_t * alloc_Matrix(int matrix_size){
     return malloc(matrix_size * sizeof(node_t));
 }
 
+
+/*
+* Allocation du tableau de matrices des blocks diagonaux.
+*/
 node_t ** alloca_blocksDiagonaux(int nbrBlocks, int sizeBlocks[]){
     int total = 0;
     for(int i = 0; i < nbrBlocks; i++){
@@ -253,12 +290,18 @@ node_t ** alloca_blocksDiagonaux(int nbrBlocks, int sizeBlocks[]){
     return malloc(sizeof(node_t) * total);
 }
 
+/*
+* Initialisation basique de PI avec 1/taille.
+*/
 void init_PI(double PI[], int size){
     for(int i = 0; i < size; size++){
         PI[i] = (1/size);
     }
 }
 
+/*
+* Comparaison entre deux tours de l'algorithme puissance.
+*/
 int compare_prev(int matrix_size, double PREV_G[], double G[]){
     for (int i = 0; i < matrix_size; i++)
     {
@@ -269,6 +312,9 @@ int compare_prev(int matrix_size, double PREV_G[], double G[]){
     return 0;
 }
 
+/*
+* Partie multiplicative de l'algorithme de puissance.
+*/
 void compute(int size, node_t block[], double PI[]){
     double res = 0.0;
     double res_prec = 0.0;
@@ -301,6 +347,10 @@ void compute(int size, node_t block[], double PI[]){
     free(TMP);
 }
 
+
+/*
+* Normalisation d'un vecteur. Ici PI.
+*/
 void normalize(int size,double PI[]){
     double sum = 0.0;
     double leftover = 0.0;
@@ -320,6 +370,10 @@ void normalize(int size,double PI[]){
     }
 }
 
+
+/*
+* Implémentation de l'algorithme de puissance, avec normalisation. //Question 1
+*/
 void puissance_et_normalisation(double ** vecteurs_propres, node_t ** blocksDiagonaux, int nbr_blocks, int sizeBlock[]){
     for(int i = 0; i < nbr_blocks; i++){
         int ite = 0;
@@ -343,10 +397,18 @@ void puissance_et_normalisation(double ** vecteurs_propres, node_t ** blocksDiag
     }
 }
 
+
+/*
+* Initialisation des blocks diagonaux.
+* /!\ Ici il nous manque des données cruciales/!\
+*/
 void init_blocksDiagonaux(int nbr_Blocks, int sizeBlock[], node_t ** blockDiagonaux){//TODO
 
 }
 
+/*
+* Allocation mémoire pour les vecteurs propres.
+*/
 double ** alloca_vecteurs_propres(int nbrBlocks, int sizeBlocks[]){
     int total = 0;
     for(int i = 0; i < nbrBlocks; i++){
@@ -356,6 +418,9 @@ double ** alloca_vecteurs_propres(int nbrBlocks, int sizeBlocks[]){
     return malloc(sizeof(double) * total);
 }
 
+/*
+* Initialisation des vecteurs propres.
+*/
 void init_vecteurs_propres(int nbr_blocks,int sizeBlock[], double ** vecteurs_propres){
     for(int i = 0; i < nbr_blocks; i++){
         for(int j = 0; j < sizeBlock[i]; j++){
@@ -364,9 +429,11 @@ void init_vecteurs_propres(int nbr_blocks,int sizeBlock[], double ** vecteurs_pr
     }
 }
 
-void puissance_sur_matrice_de_couplage(node_t A[], int sizeA){
+/*
+* Méthode des puissance sur la matrice de couplage. //QUESTION 3
+*/
+void puissance_sur_matrice_de_couplage(node_t A[], int sizeA, double PI[]){
     int ite = 0;
-        double * PI = malloc(sizeof(double) * sizeA);
         init_PI(PI, sizeA);
         double * PREV_PI = malloc(sizeof(double) * sizeA);
         
@@ -380,10 +447,39 @@ void puissance_sur_matrice_de_couplage(node_t A[], int sizeA){
         if(ite % 250 == 0){
             printf("Puissance sur matrice couplage = %d iteration \n", ite);
         }
-        free(PI);
         free(PREV_PI);
 }
 
+/*
+* Calcul du PI final. Résultat de l'algorithme //QUESTION 4
+*/
+void final_PI(double ** vecteurs_propres, double PI[],int sizeBlocks[], int sizeA){
+    double res = 0.0;
+    for(int i = 0; i < sizeA; i++){
+        for(int j = 0; j < sizeBlocks[i]; j++){
+            res = res + (vecteurs_propres[i][j] * PI[i]);
+        }
+        PI[i] = res;
+        res = 0.0;
+    }
+}
+
+/*
+* Affichage du vecteur PI.
+*/
+void print_PI(int sizeA, double PI[]){
+    printf("\n ######## PI #########");
+    for(int i = 0; i < sizeA; i++){
+        printf("%lf ", PI[i]);
+    }
+    printf("\n ######## EOF PI #########");
+}
+
+/*
+* Fonction principale, ou sont regroupées les allocation et executions.
+* Le main mesure le temps d'execution de cette fonction.
+* Dans cette fonction le temps de la partie calculatoire est également mesuré pour comparaison avec Pagerank.
+*/
 void run(char * path){
     struct timeval tv3, tv4;
 
@@ -408,31 +504,39 @@ void run(char * path){
     init_blocksDiagonaux(nbr_blocks, sizeBlock, blocksDiagonaux);
     double ** vecteurs_propres = alloca_vecteurs_propres(nbr_blocks, sizeBlock);
     init_vecteurs_propres(nbr_blocks, sizeBlock, vecteurs_propres);
+    double * PI = malloc(sizeof(double) * sizeA);
     
     /*Partie calculatoire chronometrée*/
-    gettimeofday(&tv3, NULL);
-    puissance_et_normalisation(vecteurs_propres,blocksDiagonaux, nbr_blocks, sizeBlock); //Q1, un vecteur propre par block diagonal, après convergence.
-    puissance_sur_matrice_de_couplage(A, sizeA); //Q3
+    gettimeofday(&tv3, NULL); //Début du chronomètre
+    puissance_et_normalisation(vecteurs_propres, blocksDiagonaux, nbr_blocks, sizeBlock); //Q1, un vecteur propre par block diagonal, après convergence.
+    puissance_sur_matrice_de_couplage(A, sizeA, PI); //Q3
+    final_PI(vecteurs_propres, PI, sizeBlock, sizeA);//Q4
     /*calculs*/
-    gettimeofday(&tv4, NULL);
+    gettimeofday(&tv4, NULL); //Fin du chronomètre
+    print_PI(sizeA, PI);
     printf("Compute time = %f seconds \n", (double) (tv4.tv_usec - tv3.tv_usec)/ 1000000 + (double) (tv4.tv_sec - tv3.tv_sec));
     
     /*NETTOYAGE*/
     free_matrix(P, matrix_size);
     free(P);
+    free(PI);
 }
 
 
 int main(int argc, char const *argv[])
 {
     printf("STARTING \n");
+
+    // Choix du fichier à lire
     char * path = "./res/GraphesWebTest/web1.txt";
     //char * path = "./res/Stanford.txt/Stanford.txt";
     //char * path = "./res/wb_cs_stanford.txt/wb-cs-stanford.txt";
     struct timeval tv1, tv2;
-    gettimeofday(&tv1, NULL);
+    
+    gettimeofday(&tv1, NULL); //début du chronomètre
     run(path);
-    gettimeofday(&tv2, NULL);
+    gettimeofday(&tv2, NULL); //fin du chronomètre
+    
     printf("Execution time = %f seconds \n", (double) (tv2.tv_usec - tv1.tv_usec)/ 1000000 + (double) (tv2.tv_sec - tv1.tv_sec));
     printf("FIN\n");
     exit(0);
